770019ea5805d740e15957693a2e72e11dd6d911
harsh-123567 <harshgoyal2408@gmail.com>
2025-04-20 03:33:13 +0530

    final : working

--- src/models/pendingBorrowers.model.js
+++ src/models/pendingBorrowers.model.js
@@ -1,40 +1,48 @@
 import mongoose from "mongoose";
 
-const pendingBorrowerSchema = new mongoose.Schema({
+const pendingBorrowerSchema = new mongoose.Schema(
+  {
     borrowerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    lenderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
     },
     item: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     amount: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     interest: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     months: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
+    },
+    buyerWalletAddress: {
+      type: String, // Changed from ObjectId to String
+      required: true,
     },
     isClaimed: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-    lenderId : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref : 'User'
-    },
-    buyerWalletAddress:{
-        type: mongoose.Schema.Types.ObjectId
-    }
-});
+  },
+  { timestamps: true }
+);
 
-const PendingBorrower = mongoose.model("PendingBorrower", pendingBorrowerSchema);
+const PendingBorrower = mongoose.model(
+  "PendingBorrower",
+  pendingBorrowerSchema
+);
 
 export default PendingBorrower;
